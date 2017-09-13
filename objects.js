5507ef38338431a88d339c877391c222b00aa041
jordaninthewind <jordan.kline@gmail.com>
2017-09-13 16:23:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName, songTitle) {
-  delete playlist.artistName.songTitle;
+  delete artistName.songTitle;
   return playlist;
 }
